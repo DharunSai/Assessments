2ec8e41cabf5122c4c3e1acd7d3055be801d92d8
Dharun Sainath <dharunnsainath@gmail.com>
2023-07-13 13:08:49 +0530

    third

--- scripts/main.js
+++ scripts/main.js
@@ -1,74 +1,66 @@
 require.config({
-    baseUrl: 'scripts',
-    paths: {
-      'knockout': 'knockout',
-      
-      
-    },
-    
-  });
-  
-  require(['knockout'], function(ko) {
-    // ViewModel for the main application
+  baseUrl: 'scripts',
+  paths: {
+    'knockout': 'knockout',
 
 
-    function FormViewModel()
-    {
-        var self = this;
-        self.productId = ko.observable('');
-        self.name = ko.observable('');
-        self.type = ko.observable('');
-        self.quantity = ko.observable('');
-        self.availability = ko.observable('');
-        self.discountTypes = ko.observableArray([]);
+  },
+
+});
+
+require(['knockout'], function (ko) {
+
+
+
+  function FormViewModel() {
+    let self = this;
+    self.productId = ko.observable('');
+    self.productName = ko.observable('');
+    self.productType = ko.observable('');
+    self.productQuantity = ko.observable('');
+    self.productAvailability = ko.observable('');
+    self.discountTypes = ko.observableArray([]);
+
+
+  }
+  function AppViewModel() {
+    let self = this;
+    self.isSubmit = ko.observable(false)
+
+
+    self.forms = ko.observableArray([new FormViewModel()])
+
+
+    self.addForm = function () {
+
+      self.forms.push(new FormViewModel())
 
 
     }
-    function AppViewModel() {
-        let self = this;
-        self.isSubmit = ko.observable(false)
 
+    self.removeProduct = function (product) {
 
-        self.forms = ko.observableArray([new FormViewModel()])
-
-
-        self.addForm = function(){
-
-        self.forms.push(new FormViewModel())
-
-        
-        }
-
-        self.removeProduct = function(product){
-
-        self.forms.remove(product)
-         }
-
-        self.submitForm = function()
-        {
-
-          self.isSubmit(true)
-        }
-
-        self.resetForm = function()
-        {
-          self.forms([])
-          
-        }
-
-
-  
-     
+      self.forms.remove(product)
     }
-    // Apply bindings and initialize the main application
 
-    
-  
-    // Register the product-form component
- 
-  
-        var appViewModel = new AppViewModel();
-        ko.applyBindings(appViewModel);
-  });
-  
-  // <button type="button" data-bind="click: addProduct">Add</button>
+    self.submitForm = function () {
+
+      self.isSubmit(true)
+    }
+
+    self.resetForm = function () {
+      self.forms([])
+
+    }
+
+
+
+
+  }
+
+
+
+  var appViewModel = new AppViewModel();
+  ko.applyBindings(appViewModel);
+});
+
