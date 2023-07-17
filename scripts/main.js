6e5af84e089f55c705fe3add99b3f17c642bf4cc
Dharun Sainath <dharunnsainath@gmail.com>
2023-07-17 16:54:58 +0530

    changes recommended by ajith

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+require.config({
+  baseUrl: 'scripts',
+  paths: {
+    'knockout': 'knockout',
+  },
+});
+require(['knockout'], function (ko) {
+  function FormViewModel() {
+    let self = this;
+    self.productId = ko.observable('');
+    self.productName = ko.observable('');
+    self.productType = ko.observable('');
+    self.productQuantity = ko.observable('');
+    self.productAvailability = ko.observable('');
+    self.discountTypes = ko.observableArray([]);
+  }
+  function AppViewModel() {
+    let self = this;
+    self.isSubmit = ko.observable(false)
+    self.forms = ko.observableArray([new FormViewModel()])
+    self.addForm = function () {
+        self.forms.push(new FormViewModel())
+        }
+    self.removeProduct = function (product) {
+            self.forms.remove(product)
+    }
+    self.submitForm = function () {
+      self.isSubmit(true)
+    }
+    self.resetForm = function () {
+      self.forms([])
+
+    }
+}
+  var appViewModel = new AppViewModel();
+  ko.applyBindings(appViewModel);
+});
+
